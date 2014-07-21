ed08bb7320f5455a9624a90795131be0c2cf6aa1
Nikhil Baliga <baliganikhil@gmail.com>
2014-07-21 14:29:48 +0530

    added role

--- mudra.js
+++ mudra.js
@@ -6,10 +6,20 @@ mongoose.connect('mongodb://localhost/mudra');
 
 var User = mongoose.Schema({
 	username: {type: String, required: true, index: {unique: true}},
+	name: String,
 	hash: {type: String, required: true}
 });
 
+var Role = mongoose.Schema({
+	role: {type: String, required: true, index: {unique: true}},
+	users: [{
+				username: {type: String, required: true, index: {unique: true}},
+				name: String
+			}]
+});
+
 User = mongoose.model('User', User);
+Role = mongoose.model('Role', Role);
 
 exports.register = function(payload, callback) {
 	var deferred = Q.defer();
@@ -28,7 +38,9 @@ exports.register = function(payload, callback) {
 		return deferred.promise;
 	}
 
-	create_auth_token(username, password).then(save_user, return_error);
+	create_auth_token(username, password).then(save_user, function(err) {
+		return_error(err, deferred, callback);
+	});
 
 	function save_user(hash) {
 		delete payload.password;
@@ -45,11 +57,6 @@ exports.register = function(payload, callback) {
 		});
 	}
 
-	function return_error(err) {
-		deferred.reject(err);
-		callback({status: 'error', data: err});
-	}
-
 	return deferred.promise;
 };
 
@@ -63,26 +70,20 @@ exports.login = function(payload, callback) {
 	var username = payload.username;
 	var password = payload.password;
 
-	function return_error(err) {
-		var payload = {status: 'error', data: err};
-		deferred.reject(payload);
-		callback(payload);
-	}
-
 	User.findOne({username: username}, function(err, user) {
 		if (err) {
-			return_error(err);
+			return_error(err, deferred, callback);
 			return;
 		}
 
 		if (noe(user)) {
-			return_error('No such user found');
+			return_error('No such user found', deferred, callback);
 			return;
 		}
 
 		bcrypt.compare(password, user.hash, function(err, result) {
 			if (err) {
-				return_error(err);
+				return_error(err, deferred, callback);
 				return;
 			}
 
@@ -92,7 +93,7 @@ exports.login = function(payload, callback) {
 				callback(payload);
 				deferred.resolve(payload);
 			} else {
-				return_error('Password did not match');
+				return_error('Password did not match', deferred, callback);
 			}
 
 		});
@@ -111,20 +112,14 @@ exports.authenticate = function(payload, callback) {
 	var username = payload.username;
 	var hash = payload.hash;
 
-	function return_error(err) {
-		var payload = {status: 'error', data: err};
-		deferred.reject(payload);
-		callback(payload);
-	}
-
 	User.findOne({username: username}, function(err, user) {
 		if (err) {
-			return_error(err);
+			return_error(err, deferred, callback);
 			return;
 		}
 
 		if (noe(user)) {
-			return_error('No such user found');
+			return_error('No such user found', deferred, callback);
 			return;
 		}
 
@@ -135,9 +130,69 @@ exports.authenticate = function(payload, callback) {
 			return;
 		} else {
 			var payload = 'User not authenticated';
-			return_error(payload);
+			return_error(payload, deferred, callback);
 			return;
 		}
+	});
+
+	return deferred.promise;
+};
+
+exports.create_role = function(params, callback) {
+	var deferred = Q.defer();
+
+	if (noe(callback)) {
+		callback = function() {};
+	}
+
+	(new Role(params)).save(function(err, doc) {
+        if (err) {
+        	return_error(err, deferred, callback);
+        	return false;
+        }
+
+        var payload = {status: 'success', data: doc};
+        deferred.resolve(payload);
+        callback(payload);
+	});
+
+	return deferred.promise;
+};
+
+exports.update_role = function(params, callback) {
+	var deferred = Q.defer();
+
+	if (noe(callback)) {
+		callback = function() {};
+	}
+
+	var role = params.role;
+
+	Role.findOne({role: role}, function(err, role) {
+		if (err) {
+        	return_error(err, deferred, callback);
+        	return false;
+        }
+
+        if (noe(role)) {
+			return_error('No such role found', deferred, callback);
+			return;
+		}
+
+		role = JSON.parse(JSON.stringify(role));
+        role.users = params.users;
+        delete role._id;
+
+        Role.findOneAndUpdate({role: role.role}, role, function(err) {
+	        if (err) {
+	        	return_error(err, deferred, callback);
+	        	return false;
+	        }
+
+        	var payload = {status: 'success', data: role};
+	        deferred.resolve(payload);
+	        callback(payload);
+        });
 	});
 
 	return deferred.promise;
@@ -148,7 +203,7 @@ function create_auth_token(username, password) {
 
     bcrypt.hash(password, 8, function (err, hash) {
         if (err) {
-        	deferred.reject(err);
+        	return_error(err, deferred, callback);
         	return false;
         }
 
@@ -158,8 +213,18 @@ function create_auth_token(username, password) {
 	return deferred.promise;
 }
 
+function return_error(err, deferred, callback) {
+	var payload = {status: 'error', data: err};
+	deferred.reject(payload);
+	callback(payload);
+}
+
 function noe(i) {
 	return [undefined, null, ''].indexOf(i) > -1;
 }
 
-exports.authenticate({username: 'nikhil.baliga@zovi.com', hash: '$2a$08$4Y7NNgKwZavoT8B.xy6RyuZPXOpxsitDNjq9nSlApRFh/ZAVL3WV2'}).then(function(d) {console.log(d)}, function(err) {console.log(err)});
+exports.update_role({role: 'admin', users: [{username: 'nikhil.baliga@zovi.com', name: 'Nikhil'}]}).
+then(
+		function(d) {console.log(d)},
+		function(err) {console.log(err)}
+	);
